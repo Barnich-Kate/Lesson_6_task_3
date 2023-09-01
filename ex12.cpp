2079c0ebc3b3442c98e9bc33f3ffe6e4ddf0923a
Barnich-Kate <85077866+Barnich-Kate@users.noreply.github.com>
2023-09-01 07:32:05 +0300

    Create ex12.cpp
    Add the solution of the task

---
+++ ex12.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string str = "Hello!";
+    int n = 5;
+    for (int i = 0; i < n; i++){
+        cout << str[i] << "*";
+    }
+
+    return 0;
+}
